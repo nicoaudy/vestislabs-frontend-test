0318a7b7f45264f49b51e1b0ab4925ae04d41e0c
Nico Audy <nicoaudy1994@gmail.com>
2023-08-04 11:14:21 +0700

    initial commit

---
+++ src/components/home/brands.ts
@@ -0,0 +1,54 @@
+export const brands = [
+  {
+    value: "uniqlo",
+    label: "Uniqlo",
+  },
+  {
+    value: "zara",
+    label: "Zara",
+  },
+  {
+    value: "gucci",
+    label: "Gucci",
+  },
+  {
+    value: "topman",
+    label: "Topman",
+  },
+  {
+    value: "nike",
+    label: "Nike",
+  },
+  {
+    value: "dior",
+    label: "Dior",
+  },
+  {
+    value: "balenciaga",
+    label: "Balenciaga",
+  },
+  {
+    value: "moncler",
+    label: "Moncler",
+  },
+  {
+    value: "prada",
+    label: "Prada",
+  },
+  {
+    value: "louis-vuitton",
+    label: "Louis Vuitton",
+  },
+  {
+    value: "bottega-veneta",
+    label: "Bottega Veneta",
+  },
+  {
+    value: "saint-laurent",
+    label: "Saint Laurent",
+  },
+  {
+    value: "off-white",
+    label: "Off-White",
+  },
+];
